53cdbdfe924d89f7785ee2b6a7175c3ae7d53c81
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-08-24 23:09:20 +0800

    factored out encrypt file production code.

--- src/use_cases/test_encrypt_file.py
+++ src/use_cases/test_encrypt_file.py
@@ -1,69 +1,20 @@
 import unittest
 
+from src.use_cases.encrypt_file import EncryptFileResponse, EncryptFileUseCase, EncryptFileRequest
 from src.use_cases.result import RESULT
 
 
-class EncryptFileResponse(object):
-    def __init__(self, result, output_file):
-        self._result = result
-        self._output_file = output_file
-
-    @property
-    def result(self):
-        return self._result
-
-    @property
-    def output_file(self):
-        return self._output_file
-
-    def __eq__(self, o):
-        if not isinstance(o, EncryptFileResponse):
-            return NotImplemented
-        if self is o:
-            return True
-        else:
-            return (self.result == o.result and self.output_file == o.output_file)
-
-
-class EncryptFileUseCase(object):
-    def __init__(self, cipher):
-        self.cipher = cipher
-
-    def run(self, request):
-        result = self.cipher.encrypt(request.input_file, request.output_file, request.policy_expression)
-        response = EncryptFileResponse(RESULT.SUCCESS, request.output_file)
-        return response
-
-
-class EncryptFileRequest(object):
-    def __init__(self, input_file, policy_expression, output_file=None):
-        self._input_file = input_file
-        self._policy_expression = policy_expression
-        self._output_file = output_file if output_file is not None else input_file
-
-    @property
-    def input_file(self):
-        return self._input_file
-
-    @property
-    def output_file(self):
-        return self._output_file
-
-    @property
-    def policy_expression(self):
-        return self._policy_expression
-
-
-class Cipher(object):
+class NullCipher(object):
     def encrypt(self, input_file, output_file, policy_expression):
         pass
 
 
 class EncryptFileTest(unittest.TestCase):
     def test_to_output_file(self):
-        cipher = Cipher()
-        encrypt_file = EncryptFileUseCase(cipher=cipher)
-        request = EncryptFileRequest(input_file='message.txt', policy_expression='', output_file='cipher.txt')
+        encrypt_file = EncryptFileUseCase(cipher=NullCipher())
+        request = EncryptFileRequest(input_file='message.txt',
+                                     policy_expression='',
+                                     output_file='cipher.txt')
         response = encrypt_file.run(request)
         expected_response = EncryptFileResponse(RESULT.SUCCESS, 'cipher.txt')
         self.assertEqual(expected_response, response)
