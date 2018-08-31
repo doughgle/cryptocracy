9a9a1ef837317a5f557c4d2b4db780deaa9f51e2
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-08-31 23:39:23 +0800

    specify keys as base64 encoded and >24 characters.

---
+++ src/model/key_spec.py
@@ -0,0 +1,14 @@
+import base64
+import unittest
+
+from hypothesis import given
+from hypothesis.strategies import binary
+
+keys = binary(min_size=16).map(base64.b64encode)
+
+class TestDataTypeSpecs(unittest.TestCase):
+
+    @given(keys)
+    def test_key_spec(self, key):
+        assert len(key) >= 24
+        assert base64.b64decode(key)
