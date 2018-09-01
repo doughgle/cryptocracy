02d289f66c69b2cebe7f3c6890da7a3d1db7d9a3
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-09-01 21:28:49 +0800

    factor out assert_valid method for key spec.

--- src/model/key_spec.py
+++ src/model/key_spec.py
@@ -6,9 +6,13 @@ from hypothesis.strategies import binary
 
 keys = binary(min_size=16).map(base64.b64encode)
 
-class TestDataTypeSpecs(unittest.TestCase):
 
+class TestDataTypeSpecs(unittest.TestCase):
     @given(keys)
     def test_key_spec(self, key):
-        assert len(key) >= 24
-        assert base64.b64decode(key)
+        assert_valid(key)
+
+
+def assert_valid(key):
+    assert len(key) >= 24
+    assert base64.b64decode(key)
