cbc12ba3e306b8ef5dad0348c27ab220c1263c00
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-09-09 22:10:14 +0800

    test and stub Cipher.encrypt implementation with simplecrypt library.

--- test/model/test_cipher.py
+++ test/model/test_cipher.py
@@ -1,6 +1,7 @@
 import unittest
 
-from hypothesis import given
+import simplecrypt
+from hypothesis import given, settings
 from hypothesis.strategies import binary
 
 from src.model.key_spec import keys
@@ -8,31 +9,39 @@ from src.model.policy_expression_spec import policy_expressions
 
 
 class Cipher(object):
-    def encrypt(self, message, policy_expression):
-        return message
+    def encrypt(self, plaintext, policy_expression):
+        return simplecrypt.encrypt(u'vigenere', plaintext)
 
     def proxy_decrypt(self, cloud_server_private_key, proxy_key_user, ciphertext):
         return ciphertext
 
     def decrypt(self, user_private_key, intermediate_value):
-        return intermediate_value
+        return simplecrypt.decrypt(u'vigenere', intermediate_value)
 
 
 class TestCipher(unittest.TestCase):
 
-    @given(message=binary(),
+    @settings(max_examples=3)
+    @given(binary(), policy_expressions())
+    def test_ciphertext_is_never_equal_to_plaintext(self, plaintext, policy_expression):
+        cipher = Cipher()
+        ciphertext = cipher.encrypt(plaintext, policy_expression)
+        assert ciphertext != plaintext
+
+    @settings(max_examples=3)
+    @given(plaintext=binary(),
            cloud_server_private_key=keys(),
            proxy_key_user=keys(),
            user_private_key=keys(),
            policy_expression=policy_expressions())
     def test_encrypt_proxy_decrypt_decrypt_round_trip(self,
-                                                      message,
+                                                      plaintext,
                                                       cloud_server_private_key,
                                                       proxy_key_user,
                                                       user_private_key,
                                                       policy_expression):
         cipher = Cipher()
 
-        ciphertext = cipher.encrypt(message, policy_expression)
+        ciphertext = cipher.encrypt(plaintext, policy_expression)
         intermediate_value = cipher.proxy_decrypt(cloud_server_private_key, proxy_key_user, ciphertext)
-        self.assertEqual(message, cipher.decrypt(user_private_key, intermediate_value))
+        assert plaintext == cipher.decrypt(user_private_key, intermediate_value)
