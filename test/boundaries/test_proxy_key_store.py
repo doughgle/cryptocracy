d8f9584b3697688109527282d3aa2dbdc5dc810f
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-10-02 22:48:48 +0800

    fix pylint errors.

--- test/boundaries/test_proxy_key_store.py
+++ test/boundaries/test_proxy_key_store.py
@@ -7,7 +7,11 @@ from src.boundaries.proxy_key_store import ProxyKeyStore, AwsProxyKeyStore
 from src.model.key_spec import keys
 
 
-class ProxyKeyStoreTestBase(object):
+class InMemoryProxyKeyStoreTestBase(unittest.TestCase, object):
+
+    def __init__(self, methodName, proxy_key_store=ProxyKeyStore()):
+        self.proxy_key_store = proxy_key_store
+        unittest.TestCase.__init__(self, methodName)
 
     @settings(max_examples=10)
     @given(user_id=emails(), proxy_key=keys())
@@ -18,9 +22,8 @@ class ProxyKeyStoreTestBase(object):
         self.proxy_key_store.delete(user_id)
 
 
-class InMemoryProxyKeyStoreTestBase(unittest.TestCase, ProxyKeyStoreTestBase):
-    proxy_key_store = ProxyKeyStore()
+class AwsProxyKeyStoreTestBase(InMemoryProxyKeyStoreTestBase):
 
-
-class AwsProxyKeyStoreTestBase(unittest.TestCase, ProxyKeyStoreTestBase):
-    proxy_key_store = AwsProxyKeyStore(table_name=u'proxy-key-table-nonprod')
+    def __init__(self, methodName):
+        proxy_key_store = AwsProxyKeyStore(table_name=u'proxy-key-table-nonprod')
+        InMemoryProxyKeyStoreTestBase.__init__(self, methodName, proxy_key_store)
