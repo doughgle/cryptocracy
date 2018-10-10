5588e8fc38b710a0543ff006da7e6b352ef3d410
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-10-10 22:53:06 +0800

    add revoke_user command to CLI.

--- test/use_cases/test_revoke_user.py
+++ test/use_cases/test_revoke_user.py
@@ -2,33 +2,7 @@ import unittest
 
 from src.boundaries.proxy_key_store import ProxyKeyStore
 from src.model.result import RESULT
-
-
-class RevokeUserResponse(dict):
-    pass
-    # def __init__(self):
-    #     super(RevokeUserResponse, self).__init__({"result": RESULT.SUCCESS, "user_id": "id"})
-
-
-class RevokeUserUseCase(object):
-    def __init__(self, proxy_key_store):
-        self.proxy_key_store = proxy_key_store
-
-    def run(self, request):
-        try:
-            self.proxy_key_store.delete(request.user_id)
-            return RevokeUserResponse({"result": RESULT.SUCCESS, "user_id": request.user_id})
-        except KeyError:
-            return RevokeUserResponse({"result": RESULT.FAILURE, "user_id": request.user_id, "error": "does not exist"})
-
-
-class RevokeUserRequest(object):
-    def __init__(self, user_id):
-        self._user_id = user_id
-
-    @property
-    def user_id(self):
-        return self._user_id
+from src.use_cases.revoke_user import RevokeUserUseCase, RevokeUserRequest
 
 
 class RevokeUserTest(unittest.TestCase):
