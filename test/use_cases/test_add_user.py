c6307ac92bcf8a9d05bc15d5fda96bbb50d1958b
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-10-29 22:51:15 +0800

    clean up duplicate code.

--- test/use_cases/test_add_user.py
+++ test/use_cases/test_add_user.py
@@ -2,6 +2,7 @@ import unittest
 
 from hypothesis import given
 
+from src.boundaries.proxy_key_store import ProxyKeyStore
 from src.use_cases.add_user import AddUserUseCase
 from src.model.key_spec import keys
 from src.model.result import RESULT
@@ -12,9 +13,9 @@ class AddUserTest(unittest.TestCase):
 
     @given(user_public_key=keys())
     def runTest(self, user_public_key):
-        cloud_server = CloudServerMock()
+        proxy_key_store = ProxyKeyStore()
         proxy_key_gen = ProxyKeyGeneratorMock()
-        add_user = AddUserUseCase(proxy_key_gen, cloud_server)
+        add_user = AddUserUseCase(proxy_key_gen, proxy_key_store)
 
         user_id = "alice.tan@nus.edu.sg"
         attributes = {"gender": "female", "age": 25}
@@ -22,33 +23,12 @@ class AddUserTest(unittest.TestCase):
 
         response = add_user.run(request)
 
-        self.assertIn(user_id, cloud_server.get_proxy_key_store())
         self.assertEqual(
             "dc5819e1ae1450c6044a9cc3dacc896b9d09d12f",
-            cloud_server.get(user_id)
+            proxy_key_store.get(user_id)
         )
         
         self.assertDictContainsSubset({"result": RESULT.SUCCESS, "user_id": user_id}, response)
-
-
-class CloudServerMock(object):
-
-    @property
-    def public_key(self):
-        return '4228e3eabc0ec5d246ef114eb0c11edbe6453190'
-
-    def __init__(self):
-        self.proxy_key_store = {}
-
-    def put(self, user_id, proxy_key):
-        self.proxy_key_store[user_id] = proxy_key
-
-    def get(self, user_id):
-        return self.proxy_key_store[user_id]
-
-    def get_proxy_key_store(self):
-        '''spy method'''
-        return self.proxy_key_store
 
 
 class ProxyKeyGeneratorMock(object):
