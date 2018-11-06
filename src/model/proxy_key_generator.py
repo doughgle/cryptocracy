0dda35ef5a2735809cc256aa05d656411c1fb4c6
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-11-06 22:49:42 +0800

    wrap ABE cipher for proxy key generation.

--- src/model/proxy_key_generator.py
+++ src/model/proxy_key_generator.py
@@ -1,11 +1,14 @@
-import base64
-import os
+from src.model.cipher import NullCipher
 
 
 class ProxyKeyGenerator(object):
 
-    def __init__(self, master_secret_key):
-        pass
+    def __init__(self, cipher=NullCipher()):
+        self.cipher = cipher
 
     def generate(self, user_public_key, cloud_server_public_key, user_attributes):
-        return base64.b64encode(os.urandom(16))
+        user_id = user_public_key
+        return self.cipher.proxy_keygen(cloud_server_public_key,
+                     user_public_key,
+                     user_id,
+                     user_attributes)
--- test/model/test_proxy_key_generator.py
+++ test/model/test_proxy_key_generator.py
@@ -1,28 +1,33 @@
 
 import unittest
-from hypothesis import given
-from hypothesis.strategies import text
 
+from hypothesis import given
+
+from src.model.cipher import OpenABECipher
 from src.model.key_spec import keys, assert_valid
+from src.model.policy_expression_spec import attributes
 from src.model.proxy_key_generator import ProxyKeyGenerator
 
 
 class TestProxyKeyGen(unittest.TestCase):
 
-    @given(msk=keys(), pkcs=keys(), pku=keys(), attribute_expr=text())
-    def test_returns_non_empty_key(self, msk, pkcs, pku, attribute_expr):
-        proxykey_user = ProxyKeyGenerator(msk).generate(pku, pkcs, attribute_expr)
+    def setUp(self):
+        self.key_gen = ProxyKeyGenerator(cipher=OpenABECipher())
+
+    @given(pkcs=keys(), pku=keys(), attribute_expr=attributes())
+    def test_returns_non_empty_key(self, pkcs, pku, attribute_expr):
+        proxykey_user = self.key_gen.generate(pku, pkcs, attribute_expr)
         self.assertTrue(proxykey_user)
 
-    @given(msk=keys(), pkcs=keys(), pku=keys(), attribute_expr=text())
-    def test_same_inputs_different_key(self, msk, pkcs, pku, attribute_expr):
-        proxykey_a = ProxyKeyGenerator(msk).generate(pku, pkcs, attribute_expr)
-        proxykey_b = ProxyKeyGenerator(msk).generate(pku, pkcs, attribute_expr)
+    @given(pkcs=keys(), pku=keys(), attribute_expr=attributes())
+    def test_same_inputs_different_key(self, pkcs, pku, attribute_expr):
+        proxykey_a = self.key_gen.generate(pku, pkcs, attribute_expr)
+        proxykey_b = self.key_gen.generate(pku, pkcs, attribute_expr)
         self.assertNotEqual(proxykey_a, proxykey_b)
 
-    @given(msk=keys(), pkcs=keys(), pku=keys(), attribute_expr=text())
-    def test_key_meets_key_spec(self, msk, pkcs, pku, attribute_expr):
-        proxy_key = ProxyKeyGenerator(msk).generate(pku, pkcs, attribute_expr)
+    @given(pkcs=keys(), pku=keys(), attribute_expr=attributes())
+    def test_key_meets_key_spec(self, pkcs, pku, attribute_expr):
+        proxy_key = self.key_gen.generate(pku, pkcs, attribute_expr)
         assert_valid(proxy_key)
 
 
