383b8bd78db89163f16b836dcc917aab3f2aca57
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-03-11 21:57:48 +0800

    implement key and data encapsulation using hybrid encryption.

--- src/model/cipher.py
+++ src/model/cipher.py
@@ -1,5 +1,8 @@
+from charm.core.engine.util import objectToBytes, bytesToObject
+from charm.core.math.pairing import hashPair as sha2, GT
 from charm.schemes.abenc.abenc_yllc15 import YLLC15
 from charm.toolbox.pairinggroup import PairingGroup
+from charm.toolbox.symcrypto import AuthenticatedCryptoAbstraction
 
 
 class NullCipher(object):
@@ -30,25 +33,48 @@ class CharmABE(object):
 
     def __init__(self):
         group = PairingGroup('SS512')
-        self._abe = YLLC15(group)
+        self._cpabe = YLLC15(group)
 
     def setup(self):
-        return self._abe.setup()
+        self._params, self._msk = self._cpabe.setup()
+        return self._params, self._msk
 
-    def user_keygen(self, pk, mk, object):
-        raise NotImplementedError
+    def user_keygen(self, user_id):
+        return self._cpabe.ukgen(self._params, user_id)
 
-    def proxy_keygen(self, cloud_server_public_key,
+    def proxy_keygen(self,
+                     cloud_server_public_key,
                      user_public_key,
                      user_id,
                      attribute_list):
-        raise NotImplementedError
+        return self._cpabe.proxy_keygen(self._params,
+                                        self._msk,
+                                        cloud_server_public_key,
+                                        user_public_key,
+                                        attribute_list)
 
     def encrypt(self, plaintext, policy_expression):
-        raise NotImplementedError
+        symm_key = self._cpabe.group.random(GT)
+        c1 = self._cpabe.encrypt(self._params, symm_key, policy_expression)
+        cipher = AuthenticatedCryptoAbstraction(sha2(symm_key))
+        c2 = cipher.encrypt(plaintext)
+        ciphertext = {'c1': c1, 'c2': c2}
+        return objectToBytes(ciphertext, self._cpabe.group)
 
     def proxy_decrypt(self, cloud_server_private_key, proxy_key_user, user_id, ciphertext):
-        return "intermediate_value"
+        ct = bytesToObject(ciphertext, self._cpabe.group)
+        c1, c2 = ct['c1'], ct['c2']
+        intermediate_value = self._cpabe.proxy_decrypt(self._params, cloud_server_private_key, proxy_key_user, c1)
+        if intermediate_value is False:
+            raise Exception("failed to decrypt!")
+        partial_ct = {'v': intermediate_value, 'c2': c2}
+        partial_ct_b64 = objectToBytes(partial_ct, self._cpabe.group)
+        return partial_ct_b64
 
-    def decrypt(self, user_private_key, intermediate_value):
-        raise NotImplementedError
+    def decrypt(self, user_private_key, partial_ct_b64):
+        partial_ct = bytesToObject(partial_ct_b64, self._cpabe.group)
+        symm_key = self._cpabe.decrypt(self._params, user_private_key, partial_ct['v'])
+        if symm_key is False:
+            raise Exception("failed to decrypt!")
+        cipher = AuthenticatedCryptoAbstraction(sha2(symm_key))
+        return cipher.decrypt(partial_ct['c2'])
--- test/model/test_cipher.py
+++ test/model/test_cipher.py
@@ -1,45 +1,40 @@
 import unittest
 
 from hypothesis import given, settings
+from hypothesis._strategies import lists
 from hypothesis.strategies import binary, emails
 
-from src.model.cipher import OpenABECipher
-from src.model.key_spec import keys
+from src.model.cipher import CharmABE as Cipher
 from src.model.policy_expression_spec import policy_expressions, attributes
 
 
 class TestCipher(unittest.TestCase):
 
-    @settings(max_examples=30)
+    @settings(max_examples=30, deadline=350)
     @given(plaintext=binary(min_size=1),
-           cloud_server_private_key=keys(),
-           cloud_server_public_key=keys(),
-           proxy_key_user=keys(),
-           user_private_key=keys(),
-           user_public_key=keys(),
            user_id=emails(),
-           attribute=attributes())
+           attribute_list=lists(attributes(), min_size=1))
     def test_encrypt_proxy_decrypt_decrypt_round_trip(self,
                                                       plaintext,
-                                                      cloud_server_public_key,
-                                                      cloud_server_private_key,
-                                                      proxy_key_user,
-                                                      user_public_key,
-                                                      user_private_key,
                                                       user_id,
-                                                      attribute):
-        cipher = OpenABECipher()
-        cipher.proxy_keygen(cloud_server_public_key, user_public_key, user_id, attribute)
-        policy_expression = attribute
+                                                      attribute_list):
+        cipher = Cipher()
+        cipher.setup()
 
-        pt = unicode(plaintext, 'latin-1')
+        pku, sku = cipher.user_keygen(user_id)
+        pkcs, skcs = cipher.user_keygen(user_id)
+        proxy_key_user = cipher.proxy_keygen(pkcs, pku, user_id, attribute_list)
+        policy_expression = attribute_list[0]
+
+        pt = str(plaintext, 'latin-1')
         ciphertext = cipher.encrypt(pt, policy_expression)
-        intermediate_value = cipher.proxy_decrypt(cloud_server_private_key, proxy_key_user, user_id, ciphertext)
-        self.assertEqual(pt, unicode(cipher.decrypt(user_private_key, intermediate_value), 'utf8'))
+        intermediate_value = cipher.proxy_decrypt(skcs, proxy_key_user, user_id, ciphertext)
+        self.assertEqual(pt, str(cipher.decrypt(sku, intermediate_value), 'utf8'))
 
     @given(binary(min_size=1), policy_expressions())
     def test_ciphertext_is_never_equal_to_plaintext(self, plaintext, policy_expression):
-        cipher = OpenABECipher()
-        pt = unicode(plaintext, 'latin-1')
+        cipher = Cipher()
+        cipher.setup()
+        pt = str(plaintext, 'latin-1')
         ciphertext = cipher.encrypt(pt, policy_expression)
         assert ciphertext != plaintext
