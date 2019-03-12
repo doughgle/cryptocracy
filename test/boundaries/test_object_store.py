b29559d1c6a02cd7c464f540f088eda3e16d103e
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-03-12 21:48:05 +0800

    extend deadline to allow for put get latency variability in S3.

--- test/boundaries/test_object_store.py
+++ test/boundaries/test_object_store.py
@@ -14,7 +14,7 @@ class ObjectStoreTest(unittest.TestCase):
         self.store = object_store
         unittest.TestCase.__init__(self, methodName)
 
-    @settings(max_examples=10)
+    @settings(max_examples=10, deadline=750)
     @given(ciphertext=binary(min_size=16, max_size=2048))
     def test_put_url_get_binary(self, ciphertext):
         self.path = os.path.join(os.path.dirname(__file__), 'cipher.txt')
@@ -25,17 +25,17 @@ class ObjectStoreTest(unittest.TestCase):
         self.store.put(source_url=self.path, key=key)
         download_url = self.store.get_download_url(key)
         data = self.store.get(download_url)
-        self.assertEquals(ciphertext, data)
+        self.assertEqual(ciphertext, data)
         self.store.delete(key)
         os.remove(self.path)
 
-    @settings(max_examples=10)
+    @settings(max_examples=10, deadline=750)
     @given(lookup_key=lookup_keys(), ciphertext=binary(min_size=16, max_size=2048))
     def test_put_binary_get_url(self, lookup_key, ciphertext):
         self.store.put_binary(lookup_key, ciphertext)
         download_url = self.store.get_download_url(lookup_key)
         downloaded_ciphertext = self.store.get(download_url)
-        self.assertEquals(ciphertext, downloaded_ciphertext)
+        self.assertEqual(ciphertext, downloaded_ciphertext)
 
 
 class AwsObjectStoreTest(ObjectStoreTest):
