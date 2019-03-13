2b4dc7e55848f82ad22f2f3caf8faa37132006ac
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-03-13 23:00:37 +0800

    add generate keypair use case.

--- src/model/abe_scheme.py
+++ src/model/abe_scheme.py
@@ -30,27 +30,35 @@ class NullCipher(object):
 
 
 class CharmHybridABE(object):
+    """
+    Outputs are serialized to base64 encoded bytes.
+    Inputs are deserialized to group elements.
+    """
 
     def __init__(self):
         group = PairingGroup('SS512')
         self._cpabe = YLLC15(group)
+        self._params, self._msk = self._cpabe.setup()
 
     def setup(self):
         self._params, self._msk = self._cpabe.setup()
         return self._params, self._msk
 
     def user_keygen(self, user_id):
-        return self._cpabe.ukgen(self._params, user_id)
+        pk, sk = self._cpabe.ukgen(self._params, user_id)
+        return objectToBytes(pk, self._cpabe.group), objectToBytes(sk, self._cpabe.group)
 
     def proxy_keygen(self,
                      cloud_server_public_key,
                      user_public_key,
                      user_id,
                      attribute_list):
+        pkcs = bytesToObject(cloud_server_public_key, self._cpabe.group)
+        pku = bytesToObject(user_public_key, self._cpabe.group)
         return self._cpabe.proxy_keygen(self._params,
                                         self._msk,
-                                        cloud_server_public_key,
-                                        user_public_key,
+                                        pkcs,
+                                        pku,
                                         attribute_list)
 
     def encrypt(self, plaintext, policy_expression):
@@ -62,9 +70,10 @@ class CharmHybridABE(object):
         return objectToBytes(ciphertext, self._cpabe.group)
 
     def proxy_decrypt(self, cloud_server_private_key, proxy_key_user, user_id, ciphertext):
+        skcs = bytesToObject(cloud_server_private_key, self._cpabe.group)
         ct = bytesToObject(ciphertext, self._cpabe.group)
         c1, c2 = ct['c1'], ct['c2']
-        intermediate_value = self._cpabe.proxy_decrypt(self._params, cloud_server_private_key, proxy_key_user, c1)
+        intermediate_value = self._cpabe.proxy_decrypt(self._params, skcs, proxy_key_user, c1)
         if intermediate_value is False:
             raise Exception("failed to decrypt!")
         partial_ct = {'v': intermediate_value, 'c2': c2}
@@ -72,8 +81,9 @@ class CharmHybridABE(object):
         return partial_ct_b64
 
     def decrypt(self, user_private_key, partial_ct_b64):
+        sku = bytesToObject(user_private_key, self._cpabe.group)
         partial_ct = bytesToObject(partial_ct_b64, self._cpabe.group)
-        symm_key = self._cpabe.decrypt(self._params, user_private_key, partial_ct['v'])
+        symm_key = self._cpabe.decrypt(self._params, sku, partial_ct['v'])
         if symm_key is False:
             raise Exception("failed to decrypt!")
         cipher = AuthenticatedCryptoAbstraction(sha2(symm_key))
