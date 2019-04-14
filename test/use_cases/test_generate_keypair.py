68c0509cea5130e1419e277e29980d1ffcafbcac
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-04-14 22:50:18 +0800

    Fix Cryptography home directory does not exist.

--- test/use_cases/test_generate_keypair.py
+++ test/use_cases/test_generate_keypair.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from src.model import key_spec
@@ -8,12 +9,16 @@ from src.use_cases.generate_key_pair import GenerateKeyPairUseCase, GenerateKeyP
 
 class GenerateKeyPairTest(unittest.TestCase):
 
+    def tearDown(self):
+        os.remove("./user.pub")
+        os.remove("./user.key")
+
     def test_generate_key_pair(self):
         abe_scheme = CharmHybridABE()
         params, msk = abe_scheme.setup()
         generate_key_pair = GenerateKeyPairUseCase(abe_scheme)
         response = generate_key_pair.run(
-            GenerateKeyPairRequest(params, "$HOME/.cryptocracy/user.pub", "$HOME/.cryptocracy/user.key"))
+            GenerateKeyPairRequest(params, "./user.pub", "./user.key"))
         self.assertEqual(RESULT.SUCCESS, response["result"])
         key_spec.assert_valid(response["public_key"])
         key_spec.assert_valid(response["secret_key"])
