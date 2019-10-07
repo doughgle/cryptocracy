239cb9a671ef32aed169b2f927947c0d0875c2b8
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-10-07 21:25:22 +0800

    Install gcc.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt-get -qq update
 
 # Install Base Tools
-apt-get install -y git wget make
+apt-get install -y git wget make gcc
 # Make sure python development tools are installed
 apt-get install -y python3-dev python3-setuptools
 # Install GMP (The GNU Multiple Precision Arithmetic Library)
